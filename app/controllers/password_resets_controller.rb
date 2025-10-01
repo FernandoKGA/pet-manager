98fb66a9294739dc39f0d29017437d04092649d9
Fernando Karchiloff <fernando.kga@gmail.com>
2025-10-01 01:58:39 -0300

    Implements password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,50 @@
+class PasswordResetsController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def new
+    @password_reset = PasswordReset.new
+  end
+
+  def create
+    @password_reset = PasswordReset.new(password_reset_params)
+    @password_reset.save
+
+    flash[:notice] = "Um link para trocar sua senha foi enviado ao seu email."
+    redirect_to login_path
+  end
+
+  def edit
+    @user = PasswordReset.find_by_valid_token(params[:id])
+    if @user
+    else
+      flash[:alert] = "Seu link de trocar de senha não é válido."
+      redirect_to new_password_reset_path
+    end
+  end
+
+  def update
+    @user = PasswordReset.find_by_valid_token(params[:id])
+    if @user
+      if @user.update(user_params)
+        flash[:notice] = "Sua senha foi atualizada."
+        redirect_to login_path
+      else
+        flash.now[:alert] = "Ocorreu um erro atualizando sua senha."
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      flash[:error] = "Seu link de trocar de senha não é válido."
+      redirect_to new_password_reset_path
+    end
+  end
+
+  private
+
+  def password_reset_params
+    params.require(:password_reset).permit(:email)
+  end
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,15 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # 
+  # Looking to send emails in production? Check out our Email API/SMTP product!
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["MAILTRAP_SB_USER"],
+    password: ENV["MAILTRAP_SB_PASSWORD"],
+    address: 'sandbox.smtp.mailtrap.io',
+    host: 'sandbox.smtp.mailtrap.io',
+    port: '2525',
+    authentication: :login
+  }
 end
