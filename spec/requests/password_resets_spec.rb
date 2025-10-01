accd8661d769f41388453a7679f7f8a9d00faf57
Fernando Karchiloff <fernando.kga@gmail.com>
2025-10-01 02:17:26 -0300

    Fix wrong name

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Passwordresets", type: :request do
+RSpec.describe "PasswordResets", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
