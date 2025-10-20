3782d01cb0dd3a88bfe243c933fa6995dab65e71
galdutro13 <71794776+galdutro13@users.noreply.github.com>
2025-10-19 21:08:09 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show, :edit, :update]
   get       '/register',  to: 'users#new', as: 'users_new'
-  
-  resources :pets, only: [:new, :create, :show, :edit, :update]
 
   get '/notification_center', to: 'notification_center#index', as: :notification_center
   get '/notification_center/:id', to: 'notification_center#show', as: :notification_entry
