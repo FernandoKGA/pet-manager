31165bc80cbbb6fec1373aa76efc513e08279bf9
gabs-montagnini-alcantara <gabrielmma@hotmail.com>
2025-11-01 20:23:41 -0300

    Fix: migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.1].define(version: 2025_10_30_211147) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_01_161048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
