d840c00b591742bae69ef1672d23eb6ddf0fefb9
“Mateusmss” <mateusaldanha@usp.br>
2025-11-08 19:44:01 -0300

    Fixing tests

--- features/step_definitions/pet_memorial_steps.rb
+++ features/step_definitions/pet_memorial_steps.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Given('I am logged into the platform') do
   @user = FactoryBot.create(:user)
   login_as(@user, scope: :user)
@@ -36,4 +38,17 @@ Then('I should see at least one pet’s name or photo displayed') do
 end
 
 Then('the date of passing should be visible') do
-  expect(page).to have_content(/Date of passing|_
+  expect(page).to have_content(/Date of passing|Passed away|Died on/i)
+end
+
+Then('I should see the message "No pets have passed away yet"') do
+  expect(page).to have_content('No pets have passed away yet')
+end
+
+Then('I should not see any photos or tribute cards') do
+  expect(page).not_to have_selector('.pet-tribute')
+end
+
+Then('I should be redirected to the "Memorial" page') do
+  expect(current_path).to eq(memorial_path)
+end
