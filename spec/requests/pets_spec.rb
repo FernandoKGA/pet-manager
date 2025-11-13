13707a4096844d5ea6c27200bebce5059b4570a5
Fernando Karchiloff <fernando.kga@gmail.com>
2025-11-12 21:18:28 -0300

    Change to SessionHelper

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe "Pets", type: :request do
 
   before do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-    allow_any_instance_of(ActionView::Base).to receive(:current_user).and_return(user)
+    allow_any_instance_of(SessionsHelper).to receive(:current_user).and_return(user)
   end
 
   describe "GET /pets/new" do
