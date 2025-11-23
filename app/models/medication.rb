2c98122fefecabd3350598be87bbf002d89467f5
“Mateusmss” <mateusaldanha@usp.br>
2025-11-23 17:52:41 -0300

    Removing the requirement for an end date.

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,7 +1,7 @@
 class Medication < ApplicationRecord
   belongs_to :pet
   
-  validates :name, :dosage, :frequency, :start_date, :end_date, presence: true
+  validates :name, :dosage, :frequency, :start_date, presence: true
   validate :end_date_cannot_be_before_start_date
 
   private
@@ -10,7 +10,7 @@ class Medication < ApplicationRecord
     return if end_date.blank? || start_date.blank?
 
     if end_date < start_date
-      errors.add("Data Fim não pode ser anterior à data de início")
+      errors.add(:end_date, "não pode ser anterior à data de início")
     end
   end
 end
